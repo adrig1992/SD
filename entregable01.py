f5fd3080d30e736af52cf5ad4d48cf7c7fb77d66
adrig1992 <adrig1992@gmail.com>
2015-03-13 12:47:54 +0100

    Cambiado print por \n

--- entregable01.py
+++ entregable01.py
@@ -1,4 +1,3 @@
 for i in range(99, 0, -1):
     print i," bottles of beer on the wall,", i," bottles of beer."
-    print "Take one down, pass it around,", i-1," bottles of beer on the wall."
-    print
+    print "Take one down, pass it around,", i-1," bottles of beer on the wall.\n"
