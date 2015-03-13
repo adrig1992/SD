e3eae4bf3037a06bfbd5e5eec8116c7249a6649a
adrig1992 <adrig1992@gmail.com>
2015-03-13 12:14:36 +0100

    Subido entregable

---
+++ entregable01.py
@@ -0,0 +1,4 @@
+for i in range(99, 0, -1):
+    print i," bottles of beer on the wall,", i," bottles of beer."
+    print "Take one down, pass it around,", i-1," bottles of beer on the wall."
+    print
